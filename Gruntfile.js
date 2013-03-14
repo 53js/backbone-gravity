3f565cbac0abeab6f9fb3390e8acccd58323163c
Hugo Mallet <hugomallet06@gmail.com>
2013-03-15 00:25:24 +0100

    Init lib

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+'use strict';
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+
+	// configurable paths
+	var conf = {
+		dist: 'build'
+	};
+
+	grunt.initConfig({
+		conf: conf,
+		clean: {
+			dist: ['.tmp', '<%= conf.dist %>/*'],
+			server: '.tmp'
+		},
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: [
+				'Gruntfile.js',
+				'helpers/{,*/}*.js',
+				'test/spec/{,*/}*.js'
+			]
+		},
+		uglify: {
+			dist: {
+				files: {
+					'backbone-gravity-helper.js': [
+						'helpers/{,*/}*.js'
+					],
+					'box2dweb.js': [
+						'lib/Box2dWeb-2.1.a.3.min.js'
+					]
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	grunt.registerTask('default', [
+		'clean',
+		'jshint',
+		'uglify'
+	]);
+};
---
+++ helpers/world-view.js
@@ -0,0 +1,115 @@
+'use strict';
+
+(function () {
+
+	var root = this;
+
+	var Backbone = root.Backbone,
+	$ = root.Backbone.$,
+	_ = root._;
+
+	var BackboneGravity = root.BackboneGravity || {};
+	BackboneGravity.Config = BackboneGravity.Config || {};
+	BackboneGravity.Config.SCALE = 30;
+	BackboneGravity.Config.interval = 1000 / 60;
+	BackboneGravity.Config.step = 1 / 60;
+
+	BackboneGravity.loop = (function () {
+		return  root.requestAnimationFrame	||
+		root.webkitRequestAnimationFrame	||
+		root.mozRequestAnimationFrame		||
+		root.oRequestAnimationFrame			||
+		root.msRequestAnimationFrame		||
+		function (/* function */ callback, /* DOMElement */ element) {
+			root.setTimeout(callback, BackboneGravity.Config.interval);
+		};
+	})();
+
+	BackboneGravity.Views = BackboneGravity.Views || {};
+
+	var WorldView = BackboneGravity.Views.WorldView = Backbone.View.extend({
+
+		bodies: [],
+
+		initialize: function () {
+			this.world = new B2.b2World(new B2.b2Vec2(0, 10), true);
+		},
+
+		createBody: function (view) {
+			var body = _.extend({}, view, Body);
+			body.createBody(this.world);
+			this.bodies[view.cid] = body;
+		},
+
+		removeBody: function (view) {
+			this.bodies[view.cid] = null;
+		},
+
+		resetBodies: function () {
+			this.bodies = null;
+		},
+
+		update: function () {
+			this.world.Step(BackboneGravity.Config.step, 10, 10);
+
+			_.each(this.bodies, _.bind(function (body) {
+				body.update();
+		    }, this));
+
+		    this.world.ClearForces();
+
+		    BackboneGravity.loop(_.bind(this.update, this));
+		}
+
+	});
+
+	var Body = BackboneGravity.Body = {
+
+		createBody: function (world) {
+			var width = this.$el.width(),
+				height = this.$el.height();
+
+			var fixDef = new B2.b2FixtureDef();
+			fixDef.density = 1.0;
+			fixDef.friction = 0.5;
+			fixDef.restitution = 0.2;
+			fixDef.shape = new B2.b2PolygonShape();
+			fixDef.shape.SetAsBox(width / BackboneGravity.Config.SCALE / 2, height / BackboneGravity.Config.SCALE / 2);
+			
+			var bodyDef = new B2.b2BodyDef();
+			bodyDef.type = B2.b2Body.b2_dynamicBody;
+			bodyDef.position.x = 0;
+			bodyDef.position.y = 0;
+			bodyDef.angle = this.angle;
+			
+			var body = this.body = this.world.world.CreateBody(bodyDef);
+			body.CreateFixture(fixDef);
+
+			return body;
+		},
+
+		getPosition: function () {
+			var position = this.body.GetPosition();
+			return {
+				x: position.x * BackboneGravity.Config.SCALE - this.$el.outerWidth() / 2,
+				y: position.y * BackboneGravity.Config.SCALE - this.$el.outerHeight() / 2
+			};
+		},
+
+		getAngle: function () {
+			return this.body.GetAngle() * 180 / Math.PI;
+		},
+
+		update: function () {
+			var position = this.getPosition();
+			this.$el.css({
+				left: position.x,
+				top: position.y,
+				transform: 'rotate(' + this.getAngle() + 'deg)'
+			});
+		}
+
+	};
+
+
+})();
